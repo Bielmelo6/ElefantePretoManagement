1dbd4cf7c5939c5abf07fc04304d54b0d453b2ef
Rogério Lacerda <rogeriolacerdadev@gmail.com>
2022-09-26 21:56:52 -0300

    MVC da classe Cliente, gerado com scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_231652) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_004645) do
+  create_table "conta", force: :cascade do |t|
+    t.integer "total"
+    t.integer "item_id", null: false
+    t.integer "cliente_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cliente_id"], name: "index_conta_on_cliente_id"
+    t.index ["item_id"], name: "index_conta_on_item_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "preco"
+    t.integer "conta_id", null: false
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conta_id"], name: "index_items_on_conta_id"
+  end
+
   create_table "pessoas", force: :cascade do |t|
     t.string "nome"
     t.string "cpf"
@@ -18,4 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_231652) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "conta", "clientes"
+  add_foreign_key "conta", "items"
+  add_foreign_key "items", "conta", column: "conta_id"
 end
