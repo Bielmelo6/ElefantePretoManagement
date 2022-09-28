64c4c186ce7e3d3fb3b196c26dee0ac2755cb38d
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 04:21:38 -0300

    Adicionando testes do model Cargo

--- test/models/cargo_test.rb
+++ test/models/cargo_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CargoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Criando cargo sem o preenchimento dos campos' do
+    cargo = Cargo.new
+    assert_not cargo.save
+  end
+
+  test 'Criando cargo com preenchimento dos campos corretos' do
+    cargo = Cargo.new funcao: 'Balconista', salario: '1600'
+    assert cargo.save
+  end
+
+  test 'Criando cargo com o salario sendo uma palavra' do
+    cargo = Cargo.new funcao: 'Gerente', salario: 'teste'
+    assert_not cargo.save
+  end
+
 end
+
