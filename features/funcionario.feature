f924e2fea3eeebf8b8190ee396985f0aeedbc86a
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-09-28 11:27:43 -0300

    segundo cenarios de funcionarios

--- features/funcionario.feature
+++ features/funcionario.feature
@@ -12,3 +12,11 @@ Feature: Funcionario
     Given estou na pagina de funcionario
     When eu preencho um funcionario de nome: 'i', cpf: '161169850-25', nascimento: '2001-02-02', email: 'test@test.com', cargo: '1' e clico cadastrar
     Then eu vejo uma mensagem de erro de nome invalido
+
+  Scenario: editar cadastro do funcionario com sucesso
+    Given estou na pagina de funcionarios
+    And o funcionario de nome: 'ivan do nascimento', cpf: '161169850-25', nascimento: '2001-02-02', email: 'test@test.com', cargo: '1' existe
+    When eu edito o nome do funcionario : 'ivan do nascimento' para :'ivan do' e clico atualizar
+    Then eu vejo uma mensagem que o funcionario foi editado com sucesso
+
+  
--- features/step_definitions/funcionario_steps.rb
+++ features/step_definitions/funcionario_steps.rb
@@ -3,6 +3,22 @@ Given('estou na pagina de funcionario') do
   expect(page).to have_content('New funcionario')
 end
 
+Given('estou na pagina de funcionarios') do
+  visit '/funcionarios'
+  expect(page).to have_current_path('/funcionarios')
+end
+
+And('o funcionario de nome: {string}, cpf: {string}, nascimento: {string}, email: {string}, cargo: {string} existe') do |nome,cpf,nascimento,email,cargo|
+  visit '/funcionarios/new'
+  fill_in 'funcionario[nome]', :with => nome
+  fill_in 'funcionario[cpf]', :with => cpf
+  fill_in 'funcionario[nascimento]', :with => nascimento
+  fill_in 'funcionario[email]', :with => email
+  first('#funcionario_cargo_id option', minimum: 1).select_option
+  click_button 'Create Funcionario'
+  expect(page).to have_content(nome)
+end
+
 When('eu preencho um funcionario de nome: {string}, cpf: {string}, nascimento: {string}, email: {string}, cargo: {string} e clico cadastrar') do |nome, cpf, nascimento, email, cargo |
   id = Cargo.where(id: cargo).first
 
@@ -14,7 +30,18 @@ When('eu preencho um funcionario de nome: {string}, cpf: {string}, nascimento: {
   click_button 'Create Funcionario'
 end
 
+When('eu edito o nome do funcionario : {string} para :{string} e clico atualizar') do |nome,novo |
+  expect(page).to have_content(nome)
+  click_on 'Edit this funcionario'
+  fill_in 'funcionario[nome]', :with => novo
+  click_button 'Update Funcionario'
+end
+
 Then('eu vejo uma mensagem que o funcionario  foi cadastrado com sucesso') do
   expect(page).to have_content('Funcionario was successfully created.')
+end
+
+Then('eu vejo uma mensagem que o funcionario foi editado com sucesso') do
+  expect(page).to have_content('Funcionario was successfully updated.')
 end
 
