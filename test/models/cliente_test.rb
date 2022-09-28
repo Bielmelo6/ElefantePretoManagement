75cc3fcca22c4a2b47197b67cf9b0416ae01b9c0
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 10:39:43 -0300

    Adicionando o teste do model Cliente

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ClienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Criando cliente sem o preenchimento dos campos' do
+    cliente = Cliente.new
+    assert_not cliente.save
+  end
+
+  test 'Criando cliente com preenchimento dos campos corretos' do
+    cliente = Cliente.new nome: 'Ana Souza', cpf: '84127420014'
+    assert cliente.save
+  end
+
+  test 'Criando cliente com o cpf invalido' do
+    cliente = Cliente.new nome: 'Ana Souza', cpf: '123'
+    assert_not cliente.save
+  end
+
 end
