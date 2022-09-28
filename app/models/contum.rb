a6822af128e415fb254399f935ef637c94430038
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 10:39:19 -0300

    Adicionando o teste do model Conta

--- app/models/contum.rb
+++ app/models/contum.rb
@@ -3,5 +3,6 @@ class Contum < ApplicationRecord
   belongs_to :item
   belongs_to :cliente
 
-  validates :total, presence: true, length: {in: 2..50}
+  validates :total, presence: true, length: {in: 1..50}
+
 end
