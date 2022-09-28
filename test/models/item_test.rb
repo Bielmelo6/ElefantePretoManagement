3258250143d0518e0a753fe9d936e6026ef43db3
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 10:40:35 -0300

    Adicionando o teste do model Item

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Criando item sem o preenchimento dos campos' do
+    item = Item.new
+    assert_not item.save
+  end
+
+  test 'Criando item com preenchimento dos campos corretos' do
+    item = Item.new produto: 'Corona', valor: '9'
+    assert item.save
+  end
+
+  test 'Criando item com string no campo valor' do
+    item = Item.new produto: 'Corona', valor: 'R$9,00'
+    assert_not item.save
+  end
+
 end
