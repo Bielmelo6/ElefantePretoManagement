c59cd4f48828c4aa419fc6338ff26f6b35936cfa
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 11:39:54 -0300

    Colocando dependent: :destroy nos models cliente e item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
 
   has_many :contums, dependent: :destroy
-  accepts_nested_attributes_for :contums, allow_destroy: true
 
   validates :produto, presence: true
   validates :valor, presence: true, numericality: {only_integer: true}
---
+++ features/step_definitions/dono_steps.rb
@@ -0,0 +1,44 @@
+Given('estou na pagina de dono') do
+  visit 'donos/new'
+  expect(page).to have_content('New dono')
+end
+
+Given('estou na pagina index de dono') do
+  visit 'donos'
+  expect(page).to have_current_path('/donos')
+end
+
+And('o dono de nome: {string}, cpf: {string}, cnpj: {string} existe') do |nome,cpf,cnpj|
+  visit 'donos/new'
+  fill_in 'dono[nome]', :with => nome
+  fill_in 'dono[cpf]', :with => cpf
+  fill_in 'dono[cnpj]', :with => cnpj
+  click_button 'Create Dono'
+  expect(page).to have_content(nome)
+end
+
+When('eu preencho o dono de nome: {string}, cpf: {string}, cnpj: {string} e clico cadastrar') do |nome, cpf,cnpj|
+  fill_in 'dono[nome]', :with => nome
+  fill_in 'dono[cpf]', :with => cpf
+  fill_in 'dono[cnpj]', :with => cnpj
+  click_button 'Create Dono'
+end
+
+When('eu edito o nome do dono : {string} para : {string} e clico atualizar') do |nome, novo|
+  expect(page).to have_content(nome)
+  click_on 'Edit this dono'
+  fill_in 'dono[nome]', :with => novo
+  click_button 'Update Dono'
+end
+
+Then('eu vejo uma mensagem que o dono foi cadastrado com sucesso') do
+  expect(page).to have_content('Dono was successfully created.')
+end
+
+Then('eu vejo uma mensagem de erro de nome invalido') do
+  assert_selector('li', text: 'Nome is too short (minimum is 2 characters)')
+end
+
+Then('eu vejo uma mensagem que o dono foi editado com sucesso') do
+  expect(page).to have_content('Dono was successfully updated.')
+end
