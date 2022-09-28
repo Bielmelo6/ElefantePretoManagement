d61ebc3f5b83347f6168e072cdff21e6f403c20d
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-09-28 10:40:03 -0300

    Adicionando o teste do model Dono

--- test/models/dono_test.rb
+++ test/models/dono_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class DonoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Criando dono sem o preenchimento dos campos' do
+    dono = Dono.new
+    assert_not dono.save
+  end
+
+  test 'Criando dono com preenchimento dos campos corretos' do
+    dono = Dono.new nome: 'Alex Camilo', cpf: '84127420014', cnpj: '82867923000100'
+    assert dono.save
+  end
+
+  test 'Criando dono com o cpf e o cnpj invalidos' do
+    dono = Dono.new nome: 'Alex Camilo', cpf: '123', cnpj: '1234'
+    assert_not dono.save
+  end
+
 end
+
