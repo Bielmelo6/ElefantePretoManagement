ebf40ee87b6086888477658eaa204b82ddd2c81e
Gabriel Melo <gabrielmelodemenezes@gmail.com>
2022-10-09 15:30:44 -0300

    alterando model de item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 
-  has_many :contums, dependent: :destroy
+  has_many :pedidos, dependent: :destroy
 
   validates :produto, presence: true, length: {in: 2..50}
   validates :valor, presence: true, numericality: {only_integer: true}
