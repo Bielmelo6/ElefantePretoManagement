e5e1caaf78805d645d6af003e575fd3d55c8b6ba
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-10-11 18:19:04 -0300

    segundo cenário do Login

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,6 +7,10 @@ And('eu clico no botao de cadastrar') do
   click_on 'Sign up'
 end
 
+And('eu clico no botao de logar') do
+  click_on 'Login'
+end
+
 When('eu preencho o dono de nome: {string}, cpf: {string}, cnpj: {string}, email: {string}, password: {string}, password_confirmation: {string} e clico cadastrar') do |nome, cpf,cnpj, email, pass,password|
   fill_in 'dono[nome]', :with => nome
   fill_in 'dono[cpf]', :with => cpf
@@ -19,4 +23,14 @@ end
 
 Then('eu vejo uma mensagem que fui cadastrado com sucesso') do
   expect(page).to have_content('Welcome! You have signed up successfully.')
+end
+
+When('eu preencho o email: {string} e senha: {string}') do |email,senha|
+  fill_in 'dono[email]', :with => email
+  fill_in 'dono[password]', :with => senha
+  click_button 'Log in'
+end
+
+Then('eu vejo uma mensagem que fui logado com sucesso') do
+  expect(page).to have_content('Signed in successfully.')
 end
