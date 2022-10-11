5a76dd604585afe22e2375e1f62bffcd4492dc2e
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-10-11 19:26:34 -0300

    Alterações nos cenários de dono

--- features/step_definitions/dono_steps.rb
+++ features/step_definitions/dono_steps.rb
@@ -9,28 +9,35 @@ Given('estou na pagina index de dono') do
 end
 
 And('o dono de nome: {string}, cpf: {string}, cnpj: {string} existe') do |nome,cpf,cnpj|
-  visit 'donos/new'
-  fill_in 'dono[nome]', :with => nome
-  fill_in 'dono[cpf]', :with => cpf
-  fill_in 'dono[cnpj]', :with => cnpj
-  click_button 'Create Dono'
   expect(page).to have_content(nome)
+  expect(page).to have_content(cpf)
+  expect(page).to have_content(cnpj)
+  click_on 'Show this dono'
 end
 
-When('eu preencho o dono de nome: {string}, cpf: {string}, cnpj: {string} e clico cadastrar') do |nome, cpf,cnpj|
-  fill_in 'dono[nome]', :with => nome
-  fill_in 'dono[cpf]', :with => cpf
-  fill_in 'dono[cnpj]', :with => cnpj
-  click_button 'Create Dono'
-end
 
 When('eu edito o nome do dono : {string} para : {string} e clico atualizar') do |nome, novo|
   expect(page).to have_content(nome)
   click_on 'Edit this dono'
   fill_in 'dono[nome]', :with => novo
-  click_button 'Update Dono'
+  fill_in 'dono[password]', :with => 'teste123'
+  fill_in 'dono[password_confirmation]', :with => 'teste123'
+  click_button 'Sign up'
 end
 
 Then('eu vejo uma mensagem que o dono foi cadastrado com sucesso') do
-  expect(page).to have_content('Dono was successfully created.')
+  expect(page).to have_content('Welcome! You have signed up successfully.')
+end
+
+Then('eu vejo uma mensagem que o dono foi editado com sucesso') do
+  expect(page).to have_content('Dono was successfully updated.')
+end
+
+When('eu removo o dono de nome: {string}') do |nome|
+  expect(page).to have_content(nome)
+  click_button "Destroy this dono"
+end
+
+Then('eu vejo uma mensagem que o dono foi removido com sucesso') do
+  expect(page).to have_content('Dono was successfully destroyed.')
 end
